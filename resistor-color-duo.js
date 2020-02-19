c3e621b5e59cda86109687567aef53d59f220ee8
Season Knight <60442342+Season-Knight@users.noreply.github.com>
2020-02-18 22:13:41 -0700

    add resistor color exercisms

---
+++ resistor-color-duo.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (colors) => {
+  const COLORS = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+  ];
+  return +colors.slice(0,2).map(c => COLORS.indexOf(c)).join('');
+};
